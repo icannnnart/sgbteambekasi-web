ccbec28e97be73edf4b430cede35701d13763475
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 22:47:37 +0700

    Auto commit user.php

--- application/views/dashboard/master/user.php
+++ application/views/dashboard/master/user.php
@@ -3,9 +3,37 @@
 	      <div class="col-md-12 grid-margin stretch-card">
 	         <div class="card">
 	            <div class="card-body">
-	               <h6 class="card-title">Master User</h6>
-	               <button type="button" class="btn btn-primary">New User</button>
-	            </div>
+                                    <h4 class="card-title">Default form</h4>
+                                    <p class="card-description">
+                                        Basic form layout
+                                    </p>
+                                    <form class="forms-sample">
+                                        <div class="form-group">
+                                            <label for="exampleInputUsername1">Username</label>
+                                            <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Email address</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword1">Password</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputConfirmPassword1">Confirm Password</label>
+                                            <input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password">
+                                        </div>
+                                        <div class="form-check">
+                                            <label class="form-check-label text-muted">
+                                                <input type="checkbox" class="form-check-input" required>
+                                                <a href="#">Remember me</a>
+                                                <i class="input-helper"></i></label>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary me-2">Submit</button>
+                                        <button class="btn btn-light">Cancel</button>
+                                    </form>
+                                </div>
 	         </div>
 	      </div>
 	</div>
