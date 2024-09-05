32db45a4b6ce36a38313547f0bbd81135545a1c8
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 20:40:31 +0700

    Auto commit footer.php

--- application/views/dashboard/layout/footer.php
+++ application/views/dashboard/layout/footer.php
@@ -2,8 +2,8 @@
                <!-- partial:../../partials/_footer.html -->
                <footer class="footer">
                   <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                     <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <?=date('Y')?>.  PAMDI. All rights reserved.</span>
-                     <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Powered By AIRTECHART</span>
+                     <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <?=date('Y')?>.  SGB TEAM BEKASI. All rights reserved.</span>
+                     <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Powered By ICANART</span>
                   </div>
                </footer>
                <!-- partial -->
