173e7b7815557b1b175acbc528874c1f722bc548
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 19:50:17 +0700

    Auto commit index.php

--- application/views/dashboard/main/index.php
+++ application/views/dashboard/main/index.php
@@ -4,7 +4,6 @@
          <div class="row">
             <div class="col-12 col-xl-8 mb-4 mb-xl-0">
                <h3 class="font-weight-bold">Selamat Datang, <?=$user->name?></h3>
-               <h6 class="font-weight-normal mb-0">Jangan lupa logout jika sudah selesai mengunakan aplikasi</h6>
             </div>
          </div>
       </div>
