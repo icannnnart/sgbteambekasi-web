57086575924192f3dac12c1f546ddbaf2021d712
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 21:45:51 +0700

    Auto commit trxkas.php

--- application/views/dashboard/main/trxkas.php
+++ application/views/dashboard/main/trxkas.php
@@ -7,18 +7,6 @@
                                     <hr>
                                     <form class="forms-sample">
                                         <div class="form-group">
-                                            <label for="exampleInputName1">Name</label>
-                                            <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail3">Email address</label>
-                                            <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputPassword4">Password</label>
-                                            <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
                                         	<label for="exampleInputPassword4">Nominal</label>
 						                    <div class="input-group">
 						                      <div class="input-group-prepend">
@@ -28,7 +16,7 @@
 						                    </div>
 						                  </div>
                                         <div class="form-group">
-                                            <label>File upload</label>
+                                            <label>Bukti</label>
                                             <input type="file" name="img" class="file-upload-default">
                                             <div class="input-group col-xs-12 d-flex align-items-center">
                                                 <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
@@ -36,14 +24,6 @@
                                                     <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
                                                 </span>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputCity1">City</label>
-                                            <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleTextarea1">Textarea</label>
-                                            <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
                                         </div>
                                         <button type="submit" class="btn btn-primary me-2">Submit</button>
                                         <button class="btn btn-light">Cancel</button>
