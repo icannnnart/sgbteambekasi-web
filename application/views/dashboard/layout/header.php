a730ef84ea73304e24d00294778a6d07d58373da
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 20:53:38 +0700

    Auto commit header.php

--- application/views/dashboard/layout/header.php
+++ application/views/dashboard/layout/header.php
@@ -30,7 +30,6 @@
        <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css" >
        <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
        <link rel="stylesheet" href="<?=base_url('assets/dashboard')?>/vendors/datatables.net-bs5/dataTables.bootstrap5.css">
-       <link rel="stylesheet" href="<?=base_url('assets/dashboard')?>/vendors/jam/jam.css">
    </head>
    <body>
       <div class="container-scroller">
