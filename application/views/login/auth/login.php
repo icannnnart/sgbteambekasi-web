a7948ecc66ee833ae3dc86e508082dd34137b087
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 21:29:27 +0700

    Auto commit login.php

--- application/views/login/auth/login.php
+++ application/views/login/auth/login.php
@@ -2,9 +2,9 @@
     <div class="container">
         <form id="loginForm" action="" method="POST">
             <picture>
-                <source media="(min-width:650px)" srcset="<?=base_url('assets/login/')?>image/pamdi.png">
-                    <source media="(min-width:465px)" srcset="<?=base_url('assets/login/')?>image/pamdi.png">
-                        <img src="<?=base_url('assets/login/')?>image/pamdi.png" alt="Logo PAMDI" style="width:210px;">
+                <source media="(min-width:650px)" srcset="<?=base_url('assets/dashboard/')?>images/putih.png">
+                    <source media="(min-width:465px)" srcset="<?=base_url('assets/dashboard/')?>images/putih.png">
+                        <img src="<?=base_url('assets/dashboard/')?>images/putih.png" alt="Logo SGB Team Bekasi" style="width:210px;">
                     </picture>
             <h2>LOGIN MEMBER</h2>
             <div class="input-field">
