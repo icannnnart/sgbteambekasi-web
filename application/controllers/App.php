004c6cfe73ec3c8177ff116690ad9a13b85f2c72
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 23:50:11 +0700

    Auto commit App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -9,7 +9,7 @@ class App extends CI_Controller {
 		$this->load->model('M_db');
 		if($this->session->userdata('logged_in') == 2){
 			redirect(site_url('auth/login/verification'));
-		}elseif($this->session->userdata('user_id') == null){
+		}elseif($this->session->userdata('logged_in') == null){
 			redirect(site_url());
 		}
 	}
@@ -17,7 +17,7 @@ class App extends CI_Controller {
 	public function index()
 	{
 		//$this->M_wa->successRegis('6285161707161','ihsan','ihsan.mmi121@gmail.com','IT Secure','Jl Raya Bekasi Tambun Utara 17510');
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Dashboard';
 		$data['sub_menu'] = '';
 		$donebayar = $this->db->query('SELECT SUM(nominal) AS total_valid FROM t_cashflow WHERE status=1')->row();
@@ -76,7 +76,7 @@ class App extends CI_Controller {
 			print_r(json_encode($response));
 			
 		} else {
-			$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+			$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 			$data['title'] = 'Master';
 			$data['sub_menu'] = 'User';
 			$data['data_user'] = $this->M_db->get_All_data('t_user');
@@ -87,7 +87,7 @@ class App extends CI_Controller {
 	}
 	public function masterForm()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Master';
 		$data['sub_menu'] = 'Form';
 		$this->load->view('dashboard/layout/header',$data);
@@ -96,7 +96,7 @@ class App extends CI_Controller {
 	}
 	public function masterRegform()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Master';
 		$data['sub_menu'] = 'Registrasi Form';
 		$data['data_registrasi'] = $this->M_db->get_All_data('t_register_form');
@@ -106,7 +106,7 @@ class App extends CI_Controller {
 	}
 	public function masterMember()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Master';
 		$data['sub_menu'] = 'Member';
 		$this->load->view('dashboard/layout/header',$data);
@@ -115,7 +115,7 @@ class App extends CI_Controller {
 	}
 	public function reportVisitor()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Report';
 		$data['sub_menu'] = 'Visitor';
 		$this->load->view('dashboard/layout/header',$data);
@@ -124,7 +124,7 @@ class App extends CI_Controller {
 	}
 	public function reportRegform()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Report';
 		$data['sub_menu'] = 'Registrasi Form';
 		$this->load->view('dashboard/layout/header',$data);
@@ -133,7 +133,7 @@ class App extends CI_Controller {
 	}
 	public function reportDpd()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Report';
 		$data['sub_menu'] = 'DPD';
 		$this->load->view('dashboard/layout/header',$data);
@@ -142,7 +142,7 @@ class App extends CI_Controller {
 	}
 	public function reportDpc()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Report';
 		$data['sub_menu'] = 'DPC';
 		$this->load->view('dashboard/layout/header',$data);
@@ -173,7 +173,7 @@ class App extends CI_Controller {
 		            $uploadFile = $uploadDir . basename($fixfilename);
 		            if (move_uploaded_file($_FILES['img']['tmp_name'], $uploadFile)) {
 		            	$datascashflow = array(
-		                	'id_user' => $this->session->userdata('logged_in'),
+		                	'id_user' => $this->session->userdata('user_id'),
 		                	'nominal' => $fixnominal,
 		                	'desc_cash' => "Bayar Uang Kas ".date('d-m-Y'),
 		                	'status' => 3,
@@ -182,7 +182,7 @@ class App extends CI_Controller {
 		                $response['message'] = 'Pembayaran kamu berhasil!';
 		            } else {
 		            	$datascashflow = array(
-		                	'id_user' => $this->session->userdata('logged_in'),
+		                	'id_user' => $this->session->userdata('user_id'),
 		                	'nominal' => $fixnominal,
 		                	'desc_cash' => "Bayar Uang Kas ".date('d-m-Y'),
 		                	'status' => 3, );
@@ -197,7 +197,7 @@ class App extends CI_Controller {
 		    }
 		    print_r(json_encode($response));
 		} else {
-			$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+			$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 			$data['title'] = 'Pembayaran Kas';
 			$data['sub_menu'] = 'BayarKas';
 			$this->load->view('dashboard/layout/header',$data);
@@ -209,7 +209,7 @@ class App extends CI_Controller {
 	}
 	public function settingAccount()
 	{
-		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('logged_in'));
+		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Setting';
 		$data['sub_menu'] = 'Account';
 		$this->load->view('dashboard/layout/header',$data);
