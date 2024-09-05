e932967f5c94e85f2e3cb22dd3d351aed6e1182d
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 23:25:04 +0700

    Auto commit routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -61,6 +61,7 @@ $route['app/dashboard'] = 'App';
 $route['app/pembayaran-kas'] = 'App/trxKas';
 $route['app/action/pembayaran-kas']['post'] = 'App/trxKas';
 $route['app/master/user'] = 'App/masterUser';
+$route['app/add/member']['post'] = 'App/masterUser';
 $route['app/master/register-pamdi'] = 'App/masterRegform';
 
 $route['management/view/register/(:any)'] = 'Preview/previewRegister/$1';
