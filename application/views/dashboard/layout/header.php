a84e4494b73280fe19418fda8307a7303ad7ead3
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 21:13:30 +0700

    Auto commit header.php

--- application/views/dashboard/layout/header.php
+++ application/views/dashboard/layout/header.php
@@ -4,7 +4,7 @@
       <!-- Required meta tags -->
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>PAMDI - <?=$title?> <?=$sub_menu?></title>
+      <title>SGBTEAM BEKASI - <?=$title?> <?=$sub_menu?></title>
       <!-- plugins:css -->
       <link rel="stylesheet" href="<?=base_url('assets/dashboard/')?>vendors/feather/feather.css">
       <link rel="stylesheet" href="<?=base_url('assets/dashboard/')?>vendors/ti-icons/css/themify-icons.css">
