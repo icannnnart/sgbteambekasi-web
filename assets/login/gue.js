7b668d927d07c905dcd6f616eaeffb8247a43cc6
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-05 18:52:18 +0700

    Auto commit gue.js

--- assets/login/gue.js
+++ assets/login/gue.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
                 document.getElementById('username').focus();
                 $('#loginForm').submit(function(e) {
-                    e.preventDefault();
-                    $.ajax({
-                        type: 'POST',
-                        url: $(this).attr('action'),
-                        data: $(this).serialize(),
-                        dataType: 'json',
-                        success: function(response) {
-                            if (response.status == 1) {
-                                 $('#content').css('visibility', 'visible');
-                                Swal.fire('Success', response.message, 'success').then((result) => {
-                                    $('#preloader').hide();
-                                   
-                                    if (result.isConfirmed) {
-                                        window.location.href = "<?=site_url('/app/dashboard')?>";
-                                    }
+                                e.preventDefault();
+                                $.ajax({
+                                                type: 'POST',
+                                                url: $(this).attr('action'),
+                                                data: $(this).serialize(),
+                                                dataType: 'json',
+                                                success: function(response) {
+                                                                if (response.status == 1) {
+                                                                                $('#content').css('visibility', 'visible');
+                                                                                Swal.fire('Success', response.message, 'success').then((result) => {
+                                                                                                $('#preloader').hide();
+
+                                                                                                if (result.isConfirmed) {
+                                                                                                                window.location.href = "<?=site_url('/app/dashboard')?>";
+                                                                                                }
+                                                                                });
+                                                                } else if (response.status == 2) {
+                                                                                $('#content').css('visibility', 'visible');
+                                                                                Swal.fire('Success', response.message, 'success').then((result) => {
+                                                                                                $('#preloader').hide();
+
+                                                                                                if (result.isConfirmed) {
+                                                                                                                window.location.href = "<?=site_url('auth/login/verification')?>";
+                                                                                                }
+                                                                                });
+                                                                } else {
+                                                                                $('#preloader').hide();
+                                                                                $('#content').css('visibility', 'visible');
+                                                                                Swal.fire('Error', response.message, 'error');
+                                                                }
+                                                }
                                 });
-                            } else if(response.status == 2) {
-                                 $('#content').css('visibility', 'visible');
-                                Swal.fire('Success', response.message, 'success').then((result) => {
-                                    $('#preloader').hide();
-                                   
-                                    if (result.isConfirmed) {
-                                        window.location.href = "<?=site_url('auth/login/verification')?>";
-                                    }
-                                });
-                            } else {
-                                $('#preloader').hide();
-                                $('#content').css('visibility', 'visible');
-                                Swal.fire('Error', response.message, 'error');
-                            }
-                        }
-                    });
                 });
-            });
+});
