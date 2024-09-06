1c2fdb78e1a8d445e25ebf29f4613a54a83f302b
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-07 05:32:43 +0700

    Auto commit index.php

--- application/views/dashboard/main/index.php
+++ application/views/dashboard/main/index.php
@@ -1,5 +1,15 @@
 <script src="<?=base_url('assets/dashboard/')?>vendors/lightgallery/js/lightgallery-all.min.js"></script>
 <div class="content-wrapper">
+
+   <div class="row">
+      <div class="col-md-12 grid-margin">
+         <div class="row">
+            <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+               <h3 class="font-weight-bold">Selamat Datang, <?=$user->name?></h3>
+            </div>
+         </div>
+      </div>
+   </div>
    <div class="row">
             <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
                <div class="card card-light-blue">
@@ -18,15 +28,6 @@
                </div>
             </div>
          </div>
-   <div class="row">
-      <div class="col-md-12 grid-margin">
-         <div class="row">
-            <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-               <h3 class="font-weight-bold">Selamat Datang, <?=$user->name?></h3>
-            </div>
-         </div>
-      </div>
-   </div>
    <div class="row">
       <div class="col-md-6 grid-margin stretch-card">
          <div class="card tale-bg">
