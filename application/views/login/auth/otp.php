f5181dd0369a9008a6a113afc7343d3e50419c75
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-06 16:19:21 +0700

    Auto commit otp.php

--- application/views/login/auth/otp.php
+++ application/views/login/auth/otp.php
@@ -1,62 +1,28 @@
 <body>
     <div class="container">
-        <form id="otpForm" action="" method="POST">
+        <form id="regisForm" action="" method="POST">
             <picture>
-                <source media="(min-width:650px)" srcset="<?=base_url('assets/login/')?>image/pamdi.png">
-                    <source media="(min-width:465px)" srcset="<?=base_url('assets/login/')?>image/pamdi.png">
-                        <img src="<?=base_url('assets/login/')?>image/pamdi.png" alt="Logo PAMDI" style="width:210px;">
+                <source media="(min-width:650px)" srcset="<?=base_url('assets/dashboard/')?>images/putih.png">
+                    <source media="(min-width:465px)" srcset="<?=base_url('assets/dashboard/')?>images/putih.png">
+                        <img src="<?=base_url('assets/dashboard/')?>images/putih.png" alt="Logo SGB Team Bekasi" style="width:210px;">
                     </picture>
-            <h2>Verification Code</h2>
-            <p style="color: black;">Please verify your login by entering the OTP code</p>
-
+            <h2>LOGIN MEMBER</h2>
             <div class="input-field">
-                <input type="text" name="otpku" id="otpku" required />
-                <label>OTP Code</label>
+                <input type="email" name="username" id="username" required />
+                <label>Enter Email</label>
+            </div>
+            <div class="input-field">
+                <input type="password" name="password" required />
+                <label>Enter Password</label>
             </div>
             <button type="submit">Log In</button>
             <div class="Create-account">
-                <a href="" id="resendcode" style="color:#6cff00">Resend Code</a>
-                <p>© <?=date('Y')?> PAMDI. Powered by <a href="#">AIRTECHART</a></p>
+                <p>© <?=date('Y')?> SGBTEAM BEKASI.</p>
             </div>
         </form>
     </div>
 </body>
-<script>
-            $(document).ready(function() {
-                document.getElementById('otpku').focus();
-                $('#otpForm').submit(function(e) {
-                    e.preventDefault();
-                    $.ajax({
-                        type: 'POST',
-                        url: $(this).attr('action'),
-                        data: $(this).serialize(),
-                        dataType: 'json',
-                        success: function(response) {
-                            if (response.status == 1) {
-                                 $('#content').css('visibility', 'visible');
-                                Swal.fire('Success', response.message, 'success').then((result) => {
-                                    $('#preloader').hide();
-                                   
-                                    if (result.isConfirmed) {
-                                        window.location.href = "<?=site_url('/app/dashboard')?>";
-                                    }
-                                });
-                            } else if(response.status == 2) {
-                                 $('#content').css('visibility', 'visible');
-                                Swal.fire('Success', response.message, 'success').then((result) => {
-                                    $('#preloader').hide();
-                                   
-                                    if (result.isConfirmed) {
-                                        window.location.href = "<?=site_url('auth/login/verification')?>";
-                                    }
-                                });
-                            } else {
-                                $('#preloader').hide();
-                                $('#content').css('visibility', 'visible');
-                                Swal.fire('Error', response.message, 'error');
-                            }
-                        }
-                    });
-                });
-            });
-        </script>
+<script type="text/javascript">
+    var baseurl = '<?=site_url('/app/dashboard')?>'
+</script>
+<script src="<?=base_url('assets/login/')?>gue.js"></script>
