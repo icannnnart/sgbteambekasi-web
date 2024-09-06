257b9263d2634a7ffeb5f23a28e516a454f16430
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-07 05:23:07 +0700

    Auto commit App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -39,7 +39,7 @@ class App extends CI_Controller {
 		$nov = $this->db->query("SELECT IFNULL(SUM(nominal), 0) AS total_valid FROM t_cashflow WHERE status=1 AND created_at LIKE '%".date('Y')."-11%'")->row();
 		$des = $this->db->query("SELECT IFNULL(SUM(nominal), 0) AS total_valid FROM t_cashflow WHERE status=1 AND created_at LIKE '%".date('Y')."-12%'")->row();
 		$data['grafik'] = [$jan->total_valid,$feb->total_valid,$mar->total_valid,$apr->total_valid,$mei->total_valid,$jun->total_valid,$jul->total_valid,$agu->total_valid,$sep->total_valid,$okt->total_valid,$nov->total_valid,$des->total_valid];
-		$data['datacashflow'] = $this->M_db->get_All_data('t_cashflow');
+		$data['datacashflow'] = $this->db->query("SELECT * FROM `t_cashflow` WHERE created_at LIKE '%".date('Y')."%' ORDER BY `t_cashflow`.`created_at` DESC");
 		$this->load->view('dashboard/layout/header',$data);
 		$this->load->view('dashboard/main/index');
 		$this->load->view('dashboard/layout/footer');
@@ -48,7 +48,7 @@ class App extends CI_Controller {
 	{
 		if ($this->session->userdata('role')==2) {
 			redirect((site_url()));
-			
+
 		}
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$names = $this->input->post('names');
