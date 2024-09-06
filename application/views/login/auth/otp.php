8961359d26b11292cfd0fe502904f9e44ba1787e
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-06 16:28:52 +0700

    Auto commit otp.php

--- application/views/login/auth/otp.php
+++ application/views/login/auth/otp.php
@@ -24,7 +24,7 @@
                 <label>Enter Address</label>
             </div>
             <div class="input-field">
-                <input type="email" name="nomer" id="nomer" required />
+                <input type="email" name="nomer" id="nomer" placeholder="62xxxxx" required />
                 <label>Enter No Whatsapp</label>
             </div>
             <div class="input-field">
