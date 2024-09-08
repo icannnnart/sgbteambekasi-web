218e32bf67ea81d7533b8b41388dd5d5f9bdb9a1
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-08 18:12:57 +0700

    Auto commit user.php

--- application/views/dashboard/master/user.php
+++ application/views/dashboard/master/user.php
@@ -84,7 +84,7 @@
                               <tr>
                                  <td><?=$aa?></td>
                                  <td>
-                                    <a class="badge badge-success">APPROVE</a>
+                                    <a class="badge badge-success" href="">APPROVE</a>
                                  </td>
                                  <td><?=$dataspending['name']?></td>
                                  <td><?=$dataspending['email']?></td>
