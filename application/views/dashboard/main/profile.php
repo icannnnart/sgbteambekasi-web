03112018c9b37955bb7581c86f58536447c7aac2
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-09 06:34:37 +0700

    Auto commit profile.php

--- application/views/dashboard/main/profile.php
+++ application/views/dashboard/main/profile.php
@@ -81,28 +81,54 @@
                                     </form>
                         </div>
                         <div class="d-flex align-items-start profile-feed-item">
-                           <img src="../../../assets/images/faces/face13.jpg" alt="profile" class="img-sm rounded-circle">
-                           <div class="ms-4">
-                              <h6>
-                                 Mason Beck
-                                 <small class="ms-4 text-muted"><i class="ti-time me-1"></i>10 hours</small>
-                              </h6>
-                              <p>
-                                 There is no better advertisement campaign that is low cost and also successful at the same
-                                 time.
-                              </p>
-                              <p class="small text-muted mt-2 mb-0">
-                                 <span>
-                                 <i class="ti-star me-1"></i>4
-                                 </span>
-                                 <span class="ms-2">
-                                 <i class="ti-comment me-1"></i>11
-                                 </span>
-                                 <span class="ms-2">
-                                 <i class="ti-share"></i>
-                                 </span>
-                              </p>
-                           </div>
+                           <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Horizontal Form</h4>
+                                    <p class="card-description">
+                                        Horizontal form layout
+                                    </p>
+                                    <form class="forms-sample">
+                                        <div class="form-group row">
+                                            <label for="exampleInputUsername2" class="col-sm-3 col-form-label">Email</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" class="form-control" id="exampleInputUsername2" placeholder="Username">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="exampleInputEmail2" class="col-sm-3 col-form-label">Email</label>
+                                            <div class="col-sm-9">
+                                                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" class="form-control" id="exampleInputMobile" placeholder="Mobile number">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="exampleInputPassword2" class="col-sm-3 col-form-label">Password</label>
+                                            <div class="col-sm-9">
+                                                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="exampleInputConfirmPassword2" class="col-sm-3 col-form-label">Re Password</label>
+                                            <div class="col-sm-9">
+                                                <input type="password" class="form-control" id="exampleInputConfirmPassword2" placeholder="Password">
+                                            </div>
+                                        </div>
+                                        <div class="form-check form-check-flat form-check-primary">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" class="form-check-input">
+                                                Remember me
+                                            <i class="input-helper"></i></label>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary me-2">Submit</button>
+                                        <button class="btn btn-light">Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
                      </div>
                   </div>
