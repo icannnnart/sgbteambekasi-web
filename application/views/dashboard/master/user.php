7d37c5020a06cb39162807990c6daa5bb0a4b524
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-08 18:11:30 +0700

    Auto commit user.php

--- application/views/dashboard/master/user.php
+++ application/views/dashboard/master/user.php
@@ -83,6 +83,15 @@
                               <?php $aa=0; foreach($pending_user as $dataspending){$aa++;?>
                               <tr>
                                  <td><?=$aa?></td>
+                                 <td>
+                                    <?php $level = $this->M_db->Get_user_by_id('t_role','id',$dataspending['role']); if ($level->id == 9){?>
+                                    <label class="badge badge-info"><?=$level->name?></label>
+                                    <?php }elseif ($level->id == 2){?>
+                                    <label class="badge badge-warning "><?=$level->name?></label>
+                                    <?php }else{?>
+                                    <label class="badge badge-warning "><?=$level->name?></label>
+                                    <?php }?>
+                                 </td>
                                  <td><?=$dataspending['name']?></td>
                                  <td><?=$dataspending['email']?></td>
                                  <td><?=$dataspending['nowa']?></td>
