c661784602ef1d379cac6cef7c4296d445acdeab
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-09 06:25:31 +0700

    Auto commit profile.php

--- application/views/dashboard/main/profile.php
+++ application/views/dashboard/main/profile.php
@@ -80,53 +80,6 @@
                               </p>
                            </div>
                         </div>
-                        <div class="d-flex align-items-start profile-feed-item">
-                           <img src="../../../assets/images/faces/face16.jpg" alt="profile" class="img-sm rounded-circle">
-                           <div class="ms-4">
-                              <h6>
-                                 Willie Stanley
-                                 <small class="ms-4 text-muted"><i class="ti-time me-1"></i>10 hours</small>
-                              </h6>
-                              <img src="../../../assets/images/samples/1280x768/12.jpg" alt="sample" class="rounded mw-100">
-                              <p class="small text-muted mt-2 mb-0">
-                                 <span>
-                                 <i class="ti-star me-1"></i>4
-                                 </span>
-                                 <span class="ms-2">
-                                 <i class="ti-comment me-1"></i>11
-                                 </span>
-                                 <span class="ms-2">
-                                 <i class="ti-share"></i>
-                                 </span>
-                              </p>
-                           </div>
-                        </div>
-                        <div class="d-flex align-items-start profile-feed-item">
-                           <img src="../../../assets/images/faces/face19.jpg" alt="profile" class="img-sm rounded-circle">
-                           <div class="ms-4">
-                              <h6>
-                                 Dylan Silva
-                                 <small class="ms-4 text-muted"><i class="ti-time me-1"></i>10 hours</small>
-                              </h6>
-                              <p>
-                                 When I first got into the online advertising business, I was looking for the magical
-                                 combination
-                                 that would put my website into the top search engine rankings
-                              </p>
-                              <img src="../../../assets/images/samples/1280x768/5.jpg" alt="sample" class="rounded mw-100">
-                              <p class="small text-muted mt-2 mb-0">
-                                 <span>
-                                 <i class="ti-star me-1"></i>4
-                                 </span>
-                                 <span class="ms-2">
-                                 <i class="ti-comment me-1"></i>11
-                                 </span>
-                                 <span class="ms-2">
-                                 <i class="ti-share"></i>
-                                 </span>
-                              </p>
-                           </div>
-                        </div>
                      </div>
                   </div>
                </div>
