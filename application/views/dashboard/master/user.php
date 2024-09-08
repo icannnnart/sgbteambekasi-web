3cf2c3ba92fd8ee34aa1fd9c4792b30a8eb1337f
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-08 18:05:43 +0700

    Auto commit user.php

--- application/views/dashboard/master/user.php
+++ application/views/dashboard/master/user.php
@@ -59,6 +59,55 @@
       </div>
    </div>
    <div class="row">
+      <div class="col-md-2 grid-margin stretch-card">
+         <div class="card position-relative">
+            <div class="card-body">
+               <h6 class="card-title">LIST USER SGB BEKASI</h6>
+               <div class="row">
+                  <div class="col-12">
+                     <div class="table-responsive">
+                        <table id="order-listing" class="table">
+                           <thead>
+                              <tr>
+                                 <th>No</th>
+                                 <th>Nama</th>
+                                 <th>Email</th>
+                                 <th>No Whatsapp</th>
+                                 <th>Alamat</th>
+                                 <th>Level</th>
+                                 <th>Action</th>
+                              </tr>
+                           </thead>
+                           <tbody>
+                              <?php $a=0; foreach($data_user as $datasuser){$a++;?>
+                              <tr>
+                                 <td><?=$a?></td>
+                                 <td><?=$datasuser['name']?></td>
+                                 <td><?=$datasuser['email']?></td>
+                                 <td><?=$datasuser['nowa']?></td>
+                                 <td><?=$datasuser['address']?></td>
+                                 <td>
+                                    <?php $level = $this->M_db->Get_user_by_id('t_role','id',$datasuser['role']); if ($level->id == 9){?>
+                                    <label class="badge badge-info"><?=$level->name?></label>
+                                    <?php }elseif ($level->id == 2){?>
+                                    <label class="badge badge-warning "><?=$level->name?></label>
+                                    <?php }else{?>
+                                    <label class="badge badge-warning "><?=$level->name?></label>
+                                    <?php }?>
+                                 </td>
+                                 <td>
+                                    <button type="button" class="btn btn-primary btn-rounded btn-icon"><i class="ti-eye"></i></button>
+                                 </td>
+                              </tr>
+                              <?php }?>
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
       <div class="col-md-12 grid-margin stretch-card">
          <div class="card position-relative">
             <div class="card-body">
