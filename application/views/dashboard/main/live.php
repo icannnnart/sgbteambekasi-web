40f01100f469177f9dc5dbe6f73bbd243bce075c
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 18:59:37 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -20,7 +20,7 @@
     <script>
         const video = document.getElementById('player');
         //const source = 'https://lebo.huminbird.cn/live/ballbar_21360.m3u8';
-        const source = 'http://ares.mnet.mk/hls/mnet-sport.m3u8';
+        const source = 'https://mavtv-mavtvglobal-1-in.samsung.wurl.tv/playlist.m3u8';
 
         // Cek dukungan HLS di browser
         if (Hls.isSupported()) {
