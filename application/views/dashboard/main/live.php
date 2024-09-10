a9a7aea4148f4306087127c622825a7d85291aa2
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 19:47:34 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -6,7 +6,7 @@
              
             <div class="card-body">
                <h4 class="card-title">Indonesia vs Australia<code>19:00 WIB</code></h4>
-               <video id="player" controls></video>
+               <video src="https://pull.dangaoka.com/live/stream-2445688_lhd.flv?auth_key=1725972666-0-0-20c27668034e656d156fc81a4c307323" controls></video>
             </div>
          </div>
       </div>
