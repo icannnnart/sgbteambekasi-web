530906d442d516f27734d6652d0cd94a2cb506c7
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 19:49:01 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -6,7 +6,11 @@
              
             <div class="card-body">
                <h4 class="card-title">Indonesia vs Australia<code>19:00 WIB</code></h4>
-               <video src="https://pull.dangaoka.com/live/stream-2445688_lhd.flv?auth_key=1725972666-0-0-20c27668034e656d156fc81a4c307323" controls></video>
+               <video id="my-video" class="video-js vjs-default-skin" controls preload="auto" width="640" height="264"
+    data-setup='{}'>
+    <source src="https://pull.dangaoka.com/live/stream-2445688_lhd.flv?auth_key=1725972666-0-0-20c27668034e656d156fc81a4c307323" type="video/flv">
+    Your browser does not support the video tag.
+  </video>
             </div>
          </div>
       </div>
