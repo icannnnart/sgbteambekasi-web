9c47d10cb081d266a08f4bdd1ad871843669c322
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 18:58:54 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -20,7 +20,7 @@
     <script>
         const video = document.getElementById('player');
         //const source = 'https://lebo.huminbird.cn/live/ballbar_21360.m3u8';
-        const source = 'https://live.cdn.joj.sk/live/andromeda/joj_sport-1080.m3u8';
+        const source = 'https://kwtsplta.cdn.mangomolo.com/spl/smil:spl.stream.smil/chunklist.m3u8';
 
         // Cek dukungan HLS di browser
         if (Hls.isSupported()) {
