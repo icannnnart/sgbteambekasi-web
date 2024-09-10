33ab10f32e68f7e3dba58c3db439e5d3444a2f0d
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-11 00:33:07 +0700

    Auto commit routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -61,6 +61,7 @@ $route['app/dashboard'] = 'App';
 $route['app/view/livestream'] = 'App/liveStream';
 $route['app/setting/profile'] = 'App/uProfile';
 $route['app/setting/change/api'] = 'App/chApi';
+$route['app/setting/request/email']['post'] = 'App/chApi';
 $route['app/pembayaran-kas'] = 'App/trxKas';
 $route['app/action/pembayaran-kas']['post'] = 'App/trxKas';
 $route['app/master/user'] = 'App/masterUser';
