83f7b59179172baa2821e0934eb37868037c05a1
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 19:49:28 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -16,26 +16,4 @@
       </div>
    </div>
 </div>
- <!-- Plyr JS -->
-    <script src="https://cdn.plyr.io/3.6.8/plyr.polyfilled.js"></script>
-    <!-- HLS.js -->
-    <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
-
-    <script>
-        const video = document.getElementById('player');
-        //const source = 'https://lebo.huminbird.cn/live/ballbar_21360.m3u8';
-        const source = 'https://pull.dangaoka.com/live/stream-2445688_lhd.flv?auth_key=1725972666-0-0-20c27668034e656d156fc81a4c307323';
-
-        // Cek dukungan HLS di browser
-        if (Hls.isSupported()) {
-            const hls = new Hls();
-            hls.loadSource(source);
-            hls.attachMedia(video);
-        } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-            // Dukungan native untuk HLS (Safari)
-            video.src = source;
-        }
-
-        // Inisialisasi Plyr
-        const player = new Plyr(video);
-    </script>
+<script src="https://vjs.zencdn.net/7.11.4/video.min.js"></script>
