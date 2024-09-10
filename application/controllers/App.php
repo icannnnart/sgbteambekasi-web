a5596475e0e32229c281b2db8dda60f8d3e209f4
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-11 00:01:32 +0700

    Auto commit App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -179,7 +179,7 @@ class App extends CI_Controller {
 		} else {
 			$dataregis = array('pwd' => $pwdnya,'is_active' => 1,'created_by' => $this->session->userdata('user_id'), );
 			$this->M_db->update_Data('t_emailsgb','id',$id,$dataregis);
-			echo "<script>if(confirm('Email berhasil di reject!')) {window.location.href = "site_url('app/email/member')"}</script>";
+			echo "<script>if(confirm('Email berhasil di reject!')) {window.location.href = ".site_url('app/email/member')."}</script>";
 		}
 		
 	}
