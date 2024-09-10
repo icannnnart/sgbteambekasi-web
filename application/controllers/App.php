2df3cf8eed97199db4c2b4f68d303d6594f001c9
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 23:55:57 +0700

    Auto commit App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -142,7 +142,7 @@ class App extends CI_Controller {
 	{
 		$data['user'] = $this->M_db->Get_user_by_id('t_user','id',$this->session->userdata('user_id'));
 		$data['title'] = 'Master';
-		$data['sub_menu'] = 'Registrasi Form';
+		$data['sub_menu'] = 'emails';
 		$data['data_registrasi'] = $this->M_db->get_All_data('t_emailsgb');
 		$this->load->view('dashboard/layout/header',$data);
 		$this->load->view('dashboard/master/email');
