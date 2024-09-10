6218805a0f12ced8f2bc733dbdaa7ba893cce711
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 23:25:01 +0700

    Auto commit App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -15,7 +15,7 @@ class App extends CI_Controller {
 	}
 	private function acakC($panjang)
 	{
-		$karakter= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%*';
+		$karakter= 'qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%*';
 		$string = '';
 		for ($i = 0; $i < $panjang; $i++) {
 			$pos = rand(0, strlen($karakter)-1);
