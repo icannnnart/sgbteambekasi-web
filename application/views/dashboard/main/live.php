ebf51b95aea3730fc41c96741fb5eb8983457de7
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 18:46:41 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -20,7 +20,7 @@
     <script>
         const video = document.getElementById('player');
         //const source = 'https://lebo.huminbird.cn/live/ballbar_21360.m3u8';
-        const source = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+        const source = 'http://ott.tvri.co.id/Content/HLS/Live/Channel(TVRI4)/index.m3u8';
 
         // Cek dukungan HLS di browser
         if (Hls.isSupported()) {
