e7b83f15f68fb8d562b5fb490a5a42d890b77936
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 18:31:56 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://cdn.plyr.io/3.6.8/plyr.css" />
 <div class="content-wrapper">
    <div class="row">
-      <div class="col-12">
+      <div class="col-12 grid-margin stretch-card">
          <div class="card">
              <h4 class="card-title">Indonesia vs Australia</h4>
             <div class="card-body">
