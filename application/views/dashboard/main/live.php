c624c5d1c8325e7059bb4d881cbacb54a1c27c06
icannnnart <97693488+icannnnart@users.noreply.github.com>
2024-09-10 20:02:50 +0700

    Auto commit live.php

--- application/views/dashboard/main/live.php
+++ application/views/dashboard/main/live.php
@@ -20,7 +20,7 @@
     <script>
         const video = document.getElementById('player');
         //const source = 'https://lebo.huminbird.cn/live/ballbar_21360.m3u8';
-        const source = 'https://lebo.huminbird.cn/live/ballbar_32600.m3u8';
+        const source = 'https://lebo.huminbird.cn/live/ballbar_21360.m3u8';
 
         // Cek dukungan HLS di browser
         if (Hls.isSupported()) {
